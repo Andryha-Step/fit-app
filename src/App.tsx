e8343e58834564c7e8618f240b816e6b5f8ed305
Acex187x <34621447+Acex187x@users.noreply.github.com>
2021-03-13 15:03:53 +0100

    Working onboard swiper
    Configured Swiper for working state on the onboarding screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,62 @@
 import React from 'react';
 import './App.css';
+
+// onboarding
 import { Button } from './components/Button'
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Title } from './components/Title'
 import { Text } from './components/Text'
 import logo_img from './assets/images/logo.svg'
 import powered_by_logo_img from './assets/images/poweredby_logo.svg'
 import background from './assets/backgrounds/onboarding.svg'
+import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 
+SwiperCore.use([Pagination]);
+
 
 function App() {
+
+  const SwiperCofing = {
+    containerClass: 'swiper-custom',
+    pagination: {
+      clickable: true
+    }
+  }
+
   return (
     <AppContainer>
+      <SpaceForSliderDots />
       <Backdrop>
         <PageContainer>
           <Header>
             <img width='47%' src={logo_img} alt='Moove logo' />
           </Header>
-          {/* <Slider
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            <SwiperSlide>
-              <Slide>
-                <Title center>This is swipeable</Title>
-                <Text center>I will add dots in next commit<br />And I think we need hover effect for button in desktop</Text>
-              </Slide>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Slide>
-                <Title center>ON-DEMAND AND LIVE CLASSES</Title>
-                <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
-              </Slide>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Slide>
-                <Title center>ON-DEMAND AND LIVE CLASSES</Title>
-                <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
-              </Slide>
-            </SwiperSlide>
-          </Slider>
-          <Dots>
-
-          </Dots> */}
           <Slider>
-            <Slide>
-              <Title center>ON-DEMAND AND LIVE CLASSES</Title>
-              <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
-            </Slide>
+            <Swiper
+              {...SwiperCofing}
+            >
+              <SwiperSlide>
+                <Slide>
+                  <Title center>ON-DEMAND AND LIVE CLASSES</Title>
+                  <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
+                </Slide>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide>
+                  <Title center>ON-DEMAND AND LIVE CLASSES</Title>
+                  <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
+                </Slide>
+             </SwiperSlide>
+             <SwiperSlide>
+                <Slide>
+                  <Title center>ON-DEMAND AND LIVE CLASSES</Title>
+                  <Text center>Get access to unlimited on-demand and live classes tailored to your goals. <br />Take individual classes at your convenience or schedule classes with your friends.</Text>
+                </Slide>
+             </SwiperSlide>
+            </Swiper>
           </Slider>
-          <Dots>
-
-          </Dots>
           <ButtonGroup>
             <Button primary>get started</Button>
             <Button transparent>log in</Button>
@@ -131,14 +132,14 @@ const Backdrop = styled.div`
 `
 
 const Header = styled.header`
-  flex: 1.8;
+  flex: 1.5;
   display: flex;
   justify-content: center;
   align-items: flex-end;
 `
 
 const Footer = styled.footer`
-  flex: 1.3;
+  flex: 1.5;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -151,12 +152,34 @@ const Slider = styled.div`
   flex: 3.9;
 `
 
+// Creating a new swiper-container class identical to original
+// with padding-bottom: 2rem to make space for slider pagination
+const SpaceForSliderDots = createGlobalStyle`
+  .swiper-container {
+    padding-bottom: 2.5rem;
+  }
+
+  .swiper-pagination-bullet {
+    background: white;
+    transition: width .35s .2s, opacity .35s .2s;
+    border-radius: 8px;
+    width: 9px;
+    height: 9px;
+  }
+
+  .swiper-pagination-bullet-active {
+    width: 18px;
+    border-radius: 8px;
+  }
+`
+
 const Slide = styled.div`
   display: flex;
   height: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: flex-end;
+  user-select: none;
 `
 
 const Dots = styled.div`
