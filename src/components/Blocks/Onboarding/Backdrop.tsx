69ce762a3420e2a689d1ab7758090f7faf18233f
Acex187x <34621447+Acex187x@users.noreply.github.com>
2021-03-20 11:30:14 +0100

    Remove firebase hosting + Added Div100vh lib
    Firebase hosting needs to be only locally. React-div-100vh lib is for chrome viewport fix

--- src/components/Blocks/Onboarding/Backdrop.tsx
+++ src/components/Blocks/Onboarding/Backdrop.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import Div100vh from 'react-div-100vh'
 
-export default styled.div<{blur?: boolean}>`
+export default styled(Div100vh)<{blur?: boolean}>`
 	width: 100%;
     height: 100vh;
 	/* min-height: -webkit-fill-available; */
